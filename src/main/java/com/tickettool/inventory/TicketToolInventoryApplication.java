e2daf5785cb1a6ecff6b3ecc63a318cf96a96da4
Jan Geschwandtner <geschwandtner.jan@gmail.com>
2022-05-14 11:40:53 +0200

    initial commit

---
+++ src/main/java/com/tickettool/inventory/TicketToolInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.tickettool.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketToolInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketToolInventoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tickettool/inventory/TicketToolInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tickettool.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketToolInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
